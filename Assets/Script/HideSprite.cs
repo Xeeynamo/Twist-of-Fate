c953d9d524b7ffd6e459c02f6fc8d225e1b56a53
Xeeynamo <xeeynamo@hotmail.com>
2014-06-27 22:27:08 +0200

    Catene tra le piattaforme ed un po' di ordine nel progetto

---
+++ Assets/Script/HideSprite.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideSprite : MonoBehaviour
+{
+    Sprite previousSprite;
+    SpriteRenderer spriteRender;
+
+    bool Visible
+    {
+        get { return spriteRender.sprite != null; }
+        set { spriteRender.sprite = value ? previousSprite : null; }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        spriteRender = GetComponent<SpriteRenderer>();
+        previousSprite = spriteRender.sprite;
+        Visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
