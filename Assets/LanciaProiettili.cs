07439f13eccc82f6c9a6b70644c8935f051bdb20
Essereappassito <smash92@hotmail.it>
2014-07-01 16:17:30 +0200

    I know you're there... Target lost!
    La torta è una menzogna

---
+++ Assets/LanciaProiettili.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LanciaProiettili : MonoBehaviour {
+	public GameObject Bullet;
+	float timeShoot = 1.2f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				timeShoot -= Time.deltaTime;
+				if (timeShoot < 0) {
+						GameObject istance = (GameObject)Instantiate (Bullet, new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z), transform.rotation);	
+			timeShoot = 1.2f;		
+		}
+	}
+}
---
+++ Assets/MuoviProiettile.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MuoviProiettile : MonoBehaviour {
+	private float movimento = 500; // vellocità di spostamento del proiettile
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		this.transform.position += new Vector3 (-4, 0, 0) * Time.deltaTime;
+}
+}
