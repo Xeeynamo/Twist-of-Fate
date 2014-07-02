a558111d47931deb152f8d276833c6bddaf3a8e3
Xeeynamo <xeeynamo@hotmail.com>
2014-07-02 20:54:48 +0200

    Torretta figa multi-direzionale

--- Assets/LanciaProiettili.cs
+++ Assets/LanciaProiettili.cs
@@ -1,20 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LanciaProiettili : MonoBehaviour {
-	public GameObject Bullet;
-	float timeShoot = 1.2f;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-				timeShoot -= Time.deltaTime;
-				if (timeShoot < 0) {
-						GameObject istance = (GameObject)Instantiate (Bullet, new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z), transform.rotation);	
-			timeShoot = 1.2f;		
-		}
-	}
+public class LanciaProiettili : MonoBehaviour
+{
+    public GameObject Bullet;
+    public float TimeShoot = 1.2f;
+    public float Velocità = 4;
+    // Use this for initialization
+
+    private bool Direzione
+    {
+        get { return transform.rotation.y != 0; }
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeShoot -= Time.deltaTime;
+        if (TimeShoot < 0)
+        {
+            GameObject istance = (GameObject)Instantiate(Bullet, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), transform.rotation);
+            MuoviProiettile bullet = istance.GetComponent<MuoviProiettile>();
+            bullet.Velocità = Direzione ? +Velocità : -Velocità;
+            TimeShoot = 1.2f;
+        }
+    }
 }
--- Assets/MuoviProiettile.cs
+++ Assets/MuoviProiettile.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class MuoviProiettile : MonoBehaviour {
 	private float movimento = 500; // vellocità di spostamento del proiettile
+
+    public float Velocità;
+
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		this.transform.position += new Vector3 (-4, 0, 0) * Time.deltaTime;
+		this.transform.position += new Vector3 (Velocità, 0, 0) * Time.deltaTime;
 }
 }
