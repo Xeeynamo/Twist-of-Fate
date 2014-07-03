2e6f74f648ac0a159b70003d3e9e2034d4f9f4d2
Essereappassito <smash92@hotmail.it>
2014-07-03 12:06:29 +0200

    Oh my god, is full of fuckness

--- Assets/MuoviProiettile.cs
+++ Assets/MuoviProiettile.cs
@@ -13,5 +13,8 @@ public class MuoviProiettile : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		this.transform.position += new Vector3 (Velocità, 0, 0) * Time.deltaTime;
+
+		if (this.transform.position.x < -2.384186e-07 || this.transform.position.x >19.2)
+						Destroy (this.gameObject);
 }
 }
