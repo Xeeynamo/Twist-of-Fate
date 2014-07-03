9fcc349e17a71335213b26a63f0e79bf875fa147
Essereappassito <smash92@hotmail.it>
2014-07-03 11:06:32 +0200

    Raycharles nemico funzionante

--- Assets/Knight.cs
+++ Assets/Knight.cs
@@ -3,23 +3,24 @@ using System.Collections;
 
 public class Knight : MonoBehaviour
 {
+	private Transform trs;
     private int playerMask = 1 << 6;
 
 	// Use this for initialization
 	void Start ()
 	{
-	
+		trs = this.transform;
 	}
 
 	bool IsEnemyAround()
 	{
-		Vector2 vStart = new Vector2(0, 0);
-		Vector3 vEnd = new Vector3 (0.1f, 0, 0);
+		Vector2 vStart = new Vector2(trs.position.x, trs.position.y);
+		Vector3 dir = new Vector3 (trs.rotation.x, 0, 0);
 
-		Debug.DrawRay (vStart, vEnd);
-        if (Physics2D.Raycast(vStart, vEnd, 0.5f, playerMask) != null)
+		Debug.DrawRay (vStart, Vector3.left*0.5f,Color.blue);
+        if (Physics2D.Raycast(vStart, Vector3.left, 0.5f, playerMask) != null)
 		{
-			Debug.LogError("SDFSDSFSDFSDFS");
+			//Debug.LogError("SDFSDSFSDFSDFS");
 		}
 		return false;
 	}
