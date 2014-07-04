7c763687a795f2b31c62bfa08fcc4815d876373f
The-Diamond-Jack <marcello.mancarella@gmail.com>
2014-07-04 20:16:40 +0200

    Io Sono Quello che Bussa
    -Creata Porta (Con Prefab)
    funge ma ha 2 roblemi
    1) il personaggio l'attraversa da dietro
    2) la leva ha dei pixel bianci che dovrebbero essere trasparenti

---
+++ Assets/Activation_Script.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Activation_Script : MonoBehaviour {
+
+	private Transform playerPos;
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		playerPos = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform> ();
+		anim = this.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((Vector2.Distance (playerPos.position, transform.position) <= 1) && Input.GetKeyDown(KeyCode.X))
+			anim.SetBool ("Open", true);
+		if(!anim.GetCurrentAnimatorStateInfo(0).IsName("Base"))
+			anim.SetBool ("Open", false);
+	}
+}
