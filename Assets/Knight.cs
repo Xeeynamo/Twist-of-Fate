9db2ced675e1053e0ecdfb1f0b89bae04cee7e10
Xeeynamo <xeeynamo@hotmail.com>
2014-07-04 20:45:36 +0200

    Riprogrammato il guerriero ^^
    MALEDIZIONEIOSDFJOISDFPSODFOSPDFGAOSFASPèDFGSAFSADFSA
    Avevo perso il file e l'ho dovuto riscrivere -.-"

--- Assets/Knight.cs
+++ Assets/Knight.cs
@@ -7,6 +7,24 @@ public class Knight : MonoBehaviour
 
     PhysicsManager physManager;
 
+    public float AttackStrength = 200.0f;
+    public float Inertia = 400.0f;
+
+    public float TimeOutOfAlert = 5.0f;
+    public float TimeAlertBack = 3.0f;
+    public float TimeBeforeAttack = 0.75f;
+
+    float strength;
+    float timerOutOfAlert;
+    float timerAlertBack;
+    float timerBeforeAttack;
+
+    StateManager.State State
+    {
+        get { return physManager.State; }
+        set { physManager.State = value; }
+    }
+
     void Start()
     {
         physManager = GetComponent<PhysicsManager>();
@@ -14,20 +32,75 @@ public class Knight : MonoBehaviour
 
     void FixedUpdate()
     {
-        switch (physManager.State)
+        switch (State)
         {
-            case StateManager.State.Unpressed:
+            case StateManager.State.Stand:
                 physManager.speedX = 0;
+                State = StateManager.State.Turn;
                 break;
             case StateManager.State.Walk:
-                if (physManager.CheckNearWall())
-                    physManager.State = StateManager.State.Turn;
+                if (physManager.CheckEnemyAround())
+                {
+                    timerOutOfAlert = 0;
+                    State = StateManager.State.Alert;
+                }
+                else if (physManager.CheckNearWall())
+                    State = StateManager.State.Turn;
                 break;
             case StateManager.State.Turn:
-                Debug.Log("Knight turning");
                 physManager.Direction = !physManager.Direction;
-                physManager.speedX = 0;
                 physManager.State = StateManager.State.Walk;
+                break;
+            case StateManager.State.Alert:
+                physManager.speedX = (physManager.Direction ? physManager.WalkSpeed : -physManager.WalkSpeed) / 2.0f;
+                if (physManager.CheckEnemyNear())
+                {
+                    timerBeforeAttack = 0;
+                    State = StateManager.State.PrepareAttack;
+                }
+                else if (physManager.CheckEnemyAround())
+                {
+                    timerOutOfAlert = 0;
+                }
+                else
+                {
+                    timerOutOfAlert += Time.deltaTime;
+                    if (timerOutOfAlert >= TimeOutOfAlert)
+                        State = StateManager.State.Walk;
+                }
+                break;
+            case StateManager.State.AlertBack:
+                physManager.speedX = (physManager.Direction ? physManager.WalkSpeed : -physManager.WalkSpeed) / 2.0f;
+                if (physManager.CheckEnemyAround())
+                {
+                    physManager.speedX *= -1;
+                }
+                timerAlertBack += Time.deltaTime;
+                if (timerAlertBack >= TimeAlertBack)
+                {
+                    timerOutOfAlert = 0;
+                    State = StateManager.State.Alert;
+                }
+                break;
+            case StateManager.State.PrepareAttack:
+                physManager.speedX = 0;
+                timerBeforeAttack += Time.deltaTime;
+                if (timerBeforeAttack >= TimeBeforeAttack)
+                {
+                    strength = AttackStrength;
+                    State = StateManager.State.Attack;
+                }
+                break;
+            case StateManager.State.Attack:
+                strength -= Inertia * Time.deltaTime;
+                if (strength < 0)
+                {
+                    timerAlertBack = 0;
+                    physManager.speedX = 0;
+                    State = StateManager.State.AlertBack;
+                }
+                else
+                    physManager.speedX = physManager.Direction ? strength : -strength;
                 break;
         }
     }
