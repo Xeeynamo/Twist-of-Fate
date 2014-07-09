9899854b002d4893714a65b5c9128c89e160eb14
Essereappassito <smash92@hotmail.it>
2014-07-09 15:30:43 +0200

    Prima base dei nascondigli

---
+++ Assets/ZonaCellaRotta.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZonaCellaRotta : MonoBehaviour {
+	private int playerMask = 1 << 13;
+	private Color c;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Physics2D.Raycast (new Vector2 (this.transform.position.x + 0.2f, this.transform.position.y + 0.08f), Vector3.right, 0.65f, playerMask)) {
+						print ("Qui può nascondersi");
+						c = Color.red;
+				} else {
+						c = Color.grey;
+			print ("Qui non può nascondersi");
+				}
+		Debug.DrawRay(new Vector2 (this.transform.position.x+0.2f, this.transform.position.y + 0.08f), Vector3.right * 0.65f, c);
+	}
+}
