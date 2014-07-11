869be56d7ce3c897b22bddca5d2bfa17a419a48b
The-Diamond-Jack <marcello.mancarella@gmail.com>
2014-07-11 12:28:58 +0200

    Game Over e Grazie per aver giocato

---
+++ Assets/GameOver.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (We.Input.Defense)
+			Application.LoadLevel (2);
+	}
+}
---
+++ Assets/TimerToBase.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerToBase : MonoBehaviour {
+
+	public int maxTimer;
+	private float timer;
+	// Use this for initialization
+	void Start () {
+		timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer >= maxTimer)
+			Application.LoadLevel (0);
+		timer += Time.deltaTime;
+	}
+}
