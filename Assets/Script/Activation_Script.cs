12f6c680a28351fdefeaf90ef2c36795b8586799
Essereappassito <smash92@hotmail.it>
2014-07-14 02:27:29 +0200

    Ticchettio porta

--- Assets/Script/Activation_Script.cs
+++ Assets/Script/Activation_Script.cs
@@ -15,12 +15,14 @@ public class Activation_Script : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if ((Vector2.Distance(playerPos.position, transform.position) <= 1) && Input.GetKeyDown(KeyCode.X))
-        {
-            GameObject.FindGameObjectWithTag("MainCamera").GetComponent<FollowCamera>().ObjectPeek = door;
-            anim.SetBool("Open", true);
-        }
-		if(!anim.GetCurrentAnimatorStateInfo(0).IsName("Base"))
-			anim.SetBool ("Open", false);
+				if ((Vector2.Distance (playerPos.position, transform.position) <= 1) && Input.GetKeyDown (KeyCode.X)) {
+						GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<FollowCamera> ().ObjectPeek = door;
+						anim.SetBool ("Open", true);
+
+				}
+				if (!anim.GetCurrentAnimatorStateInfo (0).IsName ("Base")) {
+						anim.SetBool ("Open", false);
+						
+				}
+		}
 	}
-}
